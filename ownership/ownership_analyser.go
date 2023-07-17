aec8e435f57e7cd44f04cf425841dea8a966ca03
Flávio Stutz <flaviostutz@gmail.com>
2023-07-17 18:21:18 +0200

    initial goroutine structure for blame analysis

--- ownership/ownership_analyser.go
+++ ownership/ownership_analyser.go
@@ -2,8 +2,10 @@ package ownership
 
 import (
 	"errors"
+	"fmt"
 	"io/fs"
 	"regexp"
+	"sync"
 	"time"
 
 	"github.com/flaviostutz/gitwho/utils"
@@ -27,7 +29,11 @@ type AuthorLines struct {
 
 type OwnershipResult struct {
 	TotalLines  uint
-	AuthorLines []AuthorLines
+	AuthorLines map[string]int
+}
+
+type blameFileRequest struct {
+	filePath string
 }
 
 func AnalyseCodeOwnership(repo *git.Repository, opts OwnershipOptions) (OwnershipResult, error) {
@@ -50,11 +56,19 @@ func AnalyseCodeOwnership(repo *git.Repository, opts OwnershipOptions) (Ownershi
 		return result, err
 	}
 
-	// MAP - analyse each file in parallel
+	// MAP - select and analyse files in parallel goroutines
 	logrus.Debugf("Preparing a pool of workers to process file analysis in parallel")
-	// TODO prepare input and output channels
+	blameFileInputChan := make(chan blameFileRequest, 10)
+	blameFileOutputChan := make(chan OwnershipResult, 10)
 
-	logrus.Debugf("Scheduling workspace files for analysis. filesRegex=%s", opts.FilesRegex)
+	// prepare worker pool to process file analysis in parallel
+	var blameFileWaitGroup sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		blameFileWaitGroup.Add(1)
+		go blameFileWorker(blameFileInputChan, blameFileOutputChan, wt, &blameFileWaitGroup)
+	}
+
+	logrus.Debugf("Scheduling files for blame analysis. filesRegex=%s", opts.FilesRegex)
 	fre, err := regexp.Compile(opts.FilesRegex)
 	if err != nil {
 		return result, errors.New("file filter regex is invalid. err=" + err.Error())
@@ -63,15 +77,43 @@ func AnalyseCodeOwnership(repo *git.Repository, opts OwnershipOptions) (Ownershi
 	fsutil.Walk(wt.Filesystem, wt.Filesystem.Root(), func(path string, finfo fs.FileInfo, err error) error {
 		if !finfo.IsDir() && finfo.Size() < 100000 && fre.MatchString(path) {
 			totalFiles += 1
-			// TODO publish to worker channel input
+			// schedule file to be blamed by parallel workers
+			blameFileInputChan <- blameFileRequest{filePath: path}
 		}
 		return nil
 	})
+	// finished publishing request messages
+	close(blameFileInputChan)
 	logrus.Debugf("%d files scheduled for analysis", totalFiles)
 
+	// wait until all messages in blameFileInputChan chan are processed by workers
+	blameFileWaitGroup.Wait()
+	// finished publishing result messages
+	close(blameFileOutputChan)
+
 	// REDUCE - summarise counters
-	logrus.Debugf("Counting lines owned per author")
-	// TODO read and count from output channel
+	logrus.Debugf("Counting total lines owned per author")
+	summaryOwnership := OwnershipResult{TotalLines: 0, AuthorLines: make(map[string]int, 0)}
+	for fileOwnership := range blameFileOutputChan {
+		summaryOwnership.TotalLines += fileOwnership.TotalLines
+		// FIXME do this for all authors
+		summaryOwnership.AuthorLines["Flavio"] += fileOwnership.AuthorLines["Flavio"]
+	}
+
+	fmt.Printf("SUMMARY: %v\n", summaryOwnership)
 
 	return result, nil
+}
+
+// this will be run by multiple goroutines
+func blameFileWorker(blameFileInputChan chan blameFileRequest, blameFileOutputChan chan OwnershipResult, workingTree *git.Worktree, wg *sync.WaitGroup) {
+	for req := range blameFileInputChan {
+		fmt.Println(req.filePath)
+		ownershipResult := OwnershipResult{TotalLines: 0, AuthorLines: make(map[string]int, 0)}
+		//FIXME IMPLEMENT ANALYSIS
+		ownershipResult.TotalLines = 10
+		ownershipResult.AuthorLines["Flavio"] = 1
+		blameFileOutputChan <- ownershipResult
+	}
+	wg.Done()
 }
