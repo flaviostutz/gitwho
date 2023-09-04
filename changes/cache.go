4d4f08134807c3ed1368528ada57f5f6f773080c
Flávio Stutz <flaviostutz@gmail.com>
2023-09-04 17:47:33 +0200

    fix: cache key for changes

--- changes/cache.go
+++ changes/cache.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"strings"
+	"time"
 
 	"github.com/flaviostutz/gitwho/utils"
 	"github.com/sirupsen/logrus"
@@ -49,7 +50,6 @@ func SaveToCache(opts ChangesOptions, result ChangesResult) error {
 	}
 	defer cachedb.Close()
 
-	// FIXME add last commit time and current date to key
 	cacheKey := getCacheKey(opts)
 
 	b, err := json.Marshal(result)
@@ -68,7 +68,18 @@ func SaveToCache(opts ChangesOptions, result ChangesResult) error {
 }
 
 func getCacheKey(opts ChangesOptions) string {
-	return fmt.Sprintf("%s:%s:%s:%s:%s:%s:%s:%s",
+	// since/until might be "relative" to current time ("1 day ago" etc)
+	// so in cases those fields are not absolute dates, use current date day
+	// as part of the key
+	// the effect is that this cache will be valid only for one day
+	add := ""
+	_, errSince := time.Parse(time.DateOnly, opts.Since)
+	_, errUntil := time.Parse(time.DateOnly, opts.Until)
+	if errSince != nil || errUntil != nil {
+		add = time.Now().Format(time.DateOnly)
+	}
+
+	return fmt.Sprintf("%s:%s:%s:%s:%s:%s:%s:%s:%s",
 		opts.RepoDir,
 		opts.Branch,
 		opts.AuthorsRegex,
@@ -76,5 +87,6 @@ func getCacheKey(opts ChangesOptions) string {
 		opts.FilesRegex,
 		opts.FilesNotRegex,
 		opts.Since,
-		opts.Until)
+		opts.Until,
+		add)
 }
